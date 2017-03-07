092e7245d1e4f4cca88267ad9a5605c3b7a45af3
DjapelZed <maks.kushnirov@gmail.com>
2017-03-07 21:48:14 +0300

    Добавлена проверка http статуса

--- check_sites_health.py
+++ check_sites_health.py
@@ -1,11 +1,35 @@
+import requests
+import whois
+from datetime import datetime 
+
+
 def load_urls4check(path):
-    pass
+    with open(path, 'r') as file:
+    	content = file.read()
+    	urls = content.split('\n')
+    	print(urls)
+    return urls
+
 
 def is_server_respond_with_200(url):
-    pass
+    request = requests.get(url)
+    status_code = request.status_code
+    print(status_code)
+    if status_code == 200:
+    	return True
+    return False
+
 
 def get_domain_expiration_date(domain_name):
-    pass
+    domain = whois.query(domain_name)
+    domain_inspiration_date = domain['expiration_date']
+    print(domain_inspiration_date)
+
 
 if __name__ == '__main__':
-    pass
+    file_path = input('Введи путь к файлу с сайтами: ')
+    urls = load_urls4check(file_path)
+    for url in urls:
+    	print(url)
+    	http_status = is_server_respond_with_200(url)
+    	get_domain_expiration_date('google.com')
