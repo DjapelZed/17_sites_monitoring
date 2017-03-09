b8608c1a55f0ef3edb0414e8cb0526c17abecbf9
DjapelZed <maks.kushnirov@gmail.com>
2017-03-09 21:54:23 +0300

    Tabs - is evil

--- check_sites_health.py
+++ check_sites_health.py
@@ -4,49 +4,49 @@ from datetime import datetime, timedelta
 
 
 def load_urls4check(path):
-	with open(path, 'r') as file:
-		content = file.read()
-		urls = content.split('\n')
-	return urls
+    with open(path, 'r') as file:
+        content = file.read()
+        urls = content.split('\n')
+    return urls
 
 
 def is_server_respond_with_200(url):
-	request = requests.get(url)
-	status_code = request.status_code
-	if status_code == 200:
-		return True
-	return False
+    request = requests.get(url)
+    status_code = request.status_code
+    if status_code == 200:
+        return True
+    return False
 
 
 def get_domain_expiration_date(domain_name):
-	domain = whois.whois(domain_name)
-	domain_expiration_date = domain['expiration_date'][0]
-	return domain_expiration_date
+    domain = whois.whois(domain_name)
+    domain_expiration_date = domain['expiration_date'][0]
+    return domain_expiration_date
 
 
 def get_date_difference(expiration_date):
-	date_now = datetime.now()
-	date_difference = expiration_date - date_now
-	return date_difference
+    date_now = datetime.now()
+    date_difference = expiration_date - date_now
+    return date_difference
 
 
 def output_to_console(url, http_status, date_difference):
-	print()
-	print(url)
-	if http_status:
-		print('HTTP Status: OK!')
-	else:
-		print('HTTP Status: Error!')
-	print('Registration term of the domain expires after {0}.'.format(date_difference))
-	print()
-	print('#' * 80)
+    print()
+    print(url)
+    if http_status:
+        print('HTTP Status: OK!')
+    else:
+        print('HTTP Status: Error!')
+    print('Registration term of the domain expires after {0}.'.format(date_difference))
+    print()
+    print('#' * 80)
 
 
 if __name__ == '__main__':
-	file_path = input('Enter a path to the file: ')
-	urls = load_urls4check(file_path)
-	for url in urls:
-		http_status = is_server_respond_with_200(url)
-		expiration_date = get_domain_expiration_date(url)
-		date_difference = get_date_difference(expiration_date)
-		output_to_console(url, http_status, date_difference)
+    file_path = input('Enter a path to the file: ')
+    urls = load_urls4check(file_path)
+    for url in urls:
+        http_status = is_server_respond_with_200(url)
+        expiration_date = get_domain_expiration_date(url)
+        date_difference = get_date_difference(expiration_date)
+        output_to_console(url, http_status, date_difference)
